74afc5ddf7f4b6c72c77e10076278c9b99f83970
Vishal9971 <87796097+Vishal9971@users.noreply.github.com>
2023-07-17 22:55:56 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-let div = document.getElementById('add-cake-js');
+let arr = ['/images/hero-img-2.jpg', '/images/hero-img-1.jpg'];
+let index = 0;
+setInterval(function () {
+  let header = document.getElementById('header-id');
+  header.style.backgroundImage = 'url(' + arr[index] + ')';
+  index = (index + 1) % arr.length;
+}, 2000);
